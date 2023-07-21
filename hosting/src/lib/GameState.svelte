961f79dd42ec22baf030c7945cec3de5859d872d
Mahmut  Seliz <mahmutseliz-gs@hotmail.com>
2023-07-21 13:15:36 +0300

    fix: differentt approach on multiple game state

--- hosting/src/lib/GameState.svelte
+++ hosting/src/lib/GameState.svelte
@@ -4,8 +4,11 @@
     import StartScreen from "./StartScreen.svelte";
     import Scores from "./Scores.svelte";
     
+    /* APPROACH 1 
     
     let nextGameState = "start";
+    let indexOfCurrentGameState = 0;
+    let indexOfnextGameState = 1;
     
     let gameStates = [
         "start",
@@ -13,20 +16,27 @@
         "finish"
     ];
 
-    let gameState;
+    $: gameState = nextGameState;
 
     function setNextGameState() {
-        let indexOfCurrentGameState = gameStates.indexOf(gameState); 
-        let indexOfnextGameState = indexOfCurrentGameState === gameStates.length - 1 ? 0 : indexOfCurrentGameState + 1;
+        indexOfCurrentGameState = gameStates.indexOf(gameState); 
+        indexOfnextGameState = indexOfCurrentGameState === gameStates.length - 1 ? 0 : indexOfCurrentGameState + 1;
         nextGameState = gameStates[indexOfnextGameState];
         console.log('set is called', indexOfCurrentGameState, indexOfnextGameState, nextGameState)
     }
 
-    $: {
-        gameState = nextGameState;
-    }
+    */
 
-   
+    let gameStates = ["start", "game", "finish"];
+    // this gameState can be read from localStorage for better ui
+    let gameState = "start";
+    $: nextGameState = returnNextGameState(gameState)
+
+    function returnNextGameState(gameState) {
+        let indexOfCurrentGameState = gameStates.indexOf(gameState); 
+        let indexOfnextGameState = indexOfCurrentGameState === gameStates.length - 1 ? 0 : indexOfCurrentGameState + 1;
+        return gameStates[indexOfnextGameState]
+    }
 
 
 </script>
@@ -39,6 +49,14 @@
 
 {/if}
 
-<button on:click={setNextGameState}>
+<button on:click={
+    () => {
+        gameState = nextGameState;
+    }
+}>
     change game state from {gameState} to {nextGameState}
 </button>
+
+<div>
+    {gameState} {nextGameState}
+</div>
